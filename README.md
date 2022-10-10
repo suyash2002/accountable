d5b30153c60d087ff31a8aabb9abdcfc4bb71704
suyash2002 <suyashbarwal@gmail.com>
2022-10-10 21:25:32 +0530

    add `blog app`

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# accountable
+# Accountable
+>This website acts as a portal for students to publicise their complaints for the administartion to see. A beginner level CRUD Application built on Django framework with proper authentication powered by third party app `allauth`, aims to do a little towards raising complaints.
+ 
+## Tech Stack
+>Django, HTML, CSS, JS
+
+## How to locally deploy the website
+Follow the steps to get the site running on your computer:
+
+1. Locate the folder `accountable` in your terminal
+>cd accountable
+
+2. Install `django` in a virtual env
+>pipenv install django
+
+>pipenv shell
+
+3. Install the required packages
+>pip install -r requirements.txt
+
+4. Run the following command to execute the website in your server
+>python manage.py migrate
+
+>python manage.py runserver
+
+
+
+
+
+
+
--- posts/adapters.py
+++ posts/adapters.py
@@ -3,10 +3,10 @@ from django.forms import ValidationError
 
 class RestrictEmailAdapter(DefaultAccountAdapter):
     def clean_email(self,email):
-        RestrictedList = ['suyashbarwal@gmail.com']
-        if email in RestrictedList:
-            raise ValidationError('You are restricted from registering. Please contact admin.')
-        if email.split('@')[1] != "nith.ac.in":
-            raise ValidationError('You are restricted from registering. Please contact admin.')
+        # RestrictedList = ['suyashbarwal@gmail.com']
+        # if email in RestrictedList:
+        #     raise ValidationError('You are restricted from registering. Please contact admin.')
+        # if email.split('@')[1] != "nith.ac.in":
+        #     raise ValidationError('You are restricted from registering. Please contact admin.')
         return email
 
